a8591fdfc81905f5ee02a8b70397b7d65e3b003e
xubinh <xubin.huang@outlook.com>
2025-02-07 22:07:24 +0800

    ✔️ Finish task "Uthread: switching between threads (moderate)"

--- user/uthread.c
+++ user/uthread.c
@@ -10,7 +10,27 @@
 #define STACK_SIZE 8192
 #define MAX_THREAD 4
 
+struct context {
+    uint64 ra;
+    uint64 sp;
+
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
+
 struct thread {
+    struct context context;
     char stack[STACK_SIZE]; /* the thread's stack */
     int state;              /* FREE, RUNNING, RUNNABLE */
 };
@@ -57,6 +77,7 @@ void thread_schedule(void) {
          * Invoke thread_switch to switch from t to next_thread:
          * thread_switch(??, ??);
          */
+        thread_switch((uint64)t, (uint64)current_thread);
     }
     else
         next_thread = 0;
@@ -70,7 +91,8 @@ void thread_create(void (*func)()) {
             break;
     }
     t->state = RUNNABLE;
-    // YOUR CODE HERE
+    t->context.sp = (uint64)(t->stack + STACK_SIZE);
+    t->context.ra = (uint64)func;
 }
 
 void thread_yield(void) {
